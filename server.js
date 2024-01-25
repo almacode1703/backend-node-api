c960bc46b336dd61b0d33d61ea92a9225a1320bc
almacode1703 <testmygitt.2023@gmail.com>
2024-01-25 20:59:22 +0530

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-// const bodyparser = require('body-parser')
+const bodyparser = require('body-parser')
 const app = express();
 const config = require("dotenv").config();
 const port = process.env.PORT || 8001;
@@ -10,24 +10,9 @@ const cors = require('cors')
 
 app.use(cors());
 app.use(express.json())
-
-
-// app.use(bodyparser.urlencoded({ extended: true }))
-// app.use(bodyparser.json())
-
-// Establishing connection with the MONGODB
-// Connecting SB using jS promise
-
-// mongoose.connect(mongo_uri)
-// .then(()=>{
-//     console.log("DB Connected successfully")
-// })
-// .catch((error)=>{
-//     console.log(error)
-// })
+app.use(express.urlencoded({extended : false}))
 
 // Connecting to Db using async await
-
 const connectToMongo = async () => {
   try {
     await mongoose.connect(mongo_uri).then(() => {
@@ -45,7 +30,7 @@ connectToMongo();
 
 
 // Create a product route
-app.post("/product", async (req, res) => {
+app.post("/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
     res.status(200).json(product);
@@ -57,4 +42,57 @@ app.post("/product", async (req, res) => {
 });
 
 // Get All products route
+app.get("/products" , async(req, res)=>{
+    try{
+       const products = await Product.find({})
+       res.status(200).json(products)
+    }
+    catch(error){
+        res.status(500).json({message : error.message})
+    }
+})
+
+// Specific product with Id
+app.get('/products/:id', async(req, res)=>{
+    try{
+        const {id} = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product)
+    }
+    catch(error){
+        res.status(500).json({message : error.message})
+    }
+})
+
+// Edit an Existing Product
+app.put('/products/:id', async(req, res)=>{
+    try{
+      const{id} = req.params;
+      const product = await Product.findByIdAndUpdate(id, req.body)
+      if(!product){
+        return res.product(404).json({message : `can not find product with ID : ${id}`})
+      }
+      const updatedProduct = await Product.findById(id)
+      res.status(200).json(updatedProduct)
+    }
+    catch(error){
+       res.status()
+    }
+})
+
+// Delete an existing Product
+app.delete('/products/:id', async(req, res)=>{
+    try{
+      const {id} = req.params;
+      const product = await Product.findByIdAndDelete(id)
+
+      if(!product){
+        return res.status(404).json({message: "Cant find product with that Id"})
+      }
+      res.status(200).json(product)
+    }
+    catch(error){
+      res.status(500).json({message: error.message})
+    }
+})
 
